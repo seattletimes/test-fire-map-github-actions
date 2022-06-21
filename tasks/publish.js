45e04dc57ca7a44a20ab125be4f0e299d46fb84b
lflannery <lflannery@seattletimes.com>
2022-06-21 10:45:20 -0700

    console.log s3

--- tasks/publish.js
+++ tasks/publish.js
@@ -80,6 +80,8 @@ module.exports = function(grunt) {
 
     var s3 = new aws.S3();
     var uploads = findBuiltFiles();
+
+    console.log(s3);
     async.eachLimit(uploads, 10, function(upload, c) {
 
       async.waterfall([function(next) {
